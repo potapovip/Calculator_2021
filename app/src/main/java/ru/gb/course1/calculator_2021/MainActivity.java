72c947c45e7071d7f26431165e7a4b7764428596
SysAdmin <potapovip@yandex.ru>
2021-12-27 12:30:42 +0500

    Add Views

--- app/src/main/java/ru/gb/course1/calculator_2021/MainActivity.java
+++ app/src/main/java/ru/gb/course1/calculator_2021/MainActivity.java
@@ -3,6 +3,7 @@ package ru.gb.course1.calculator_2021;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +12,72 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void zero_btn(View view){
+
+    }
+    public void one_btn(View view){
+
+    }
+    public void two_btn(View view){
+
+    }
+    public void three_btn(View view){
+
+    }
+    public void four_btn(View view){
+
+    }
+    public void five_btn(View view){
+
+    }
+    public void six_btn(View view){
+
+    }
+    public void seven_btn(View view){
+
+    }
+    public void eith_btn(View view){
+
+    }
+    public void eleven_btn(View view){
+
+    }
+    public void nine_btn(View view){
+
+    }
+    public void clear_btn(View view){
+
+    }
+    public void exponent_btn(View view){
+
+    }
+    public void parentheses_btn(View view){
+
+    }
+    public void divide_btn(View view){
+
+    }
+    public void multiply_btn(View view){
+
+    }
+    public void add_btn(View view){
+
+    }
+    public void subtract_btn(View view){
+
+    }
+    public void plusMinus_btn(View view){
+
+    }
+    public void point_btn(View view){
+
+    }
+    public void equals_btn(View view){
+
+    }
+
+    public void backspace_btn(View view){
+
+    }
+
 }
